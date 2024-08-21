0cde98bb29a294764774029f7553a51ef1705d1b
ZhenisM <zhenisbtr@gmail.com>
2024-08-21 16:21:43 +0500

    add tailwindcss and vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+import '../css/app.css';
+
+import { createApp } from "vue/dist/vue.esm-bundler";
+
+import TestComponent from "./TestComponent.vue";
+
+const app = createApp({
+	components: {
+		TestComponent
+	}
+});
+
+app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path'; 
 
 export default defineConfig({
     plugins: [
+    	vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'resources/js/app.js'), // Используем resolve для корректного пути
+            },
+            output: {
+                assetFileNames: 'assets/[name]-[hash][extname]', // Настройка именования файлов
+            },
+        },
+    },
 });
