42c44f28491ecc33e3491b17120de253e873d6f2
ZhenisM <zhenisbtr@gmail.com>
2025-07-04 17:55:24 +0500

    create show page post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,4 +15,13 @@ class PostController extends Controller
     		"posts" => $posts,
     	]);
     }
+
+    public function show($id)
+    {
+    	$post = Post::findOrFail($id);
+
+    	return view('posts.show', [
+    		"post" => $post,
+    	]);
+    }
 }
