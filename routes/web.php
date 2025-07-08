5ec1ceb2a87c42219182e31b3e3b9bd345a25b60
ZhenisM <zhenisbtr@gmail.com>
2025-07-08 18:40:00 +0500

    create AuthController and all strucrure authentication

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('
 
 Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{id}', [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
+
+Route::get('/login', [\App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login');
+Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+Route::post('/login_process', [\App\Http\Controllers\AuthController::class, 'login'])->name('login_process');
+
+Route::get('/register', [\App\Http\Controllers\AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register_process', [\App\Http\Controllers\AuthController::class, 'register'])->name('register_process');
